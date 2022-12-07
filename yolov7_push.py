69687f474db65c4c53292c2ed5c73cff32db7093
doneisdone <doneisdone36@gmail.com>
2022-12-08 01:33:53 +0900

    estimation

--- yolov7_push.py
+++ yolov7_push.py
@@ -9,9 +9,24 @@ from models.experimental import attempt_load
 from utils.plots import output_to_keypoint, plot_skeleton_kpts
 from utils.general import non_max_suppression_kpt, strip_optimizer
 from torchvision import transforms
-from yolov7_push_up_util import findAngle
+from yolov7_push_util import Angle
 from PIL import Image, ImageDraw, ImageFont
 import math
+
+def IsOutlier_pushups(angle_list, joint):
+  """
+  한 프레임 차이인데 각도차이가 너무 많이 나면 그 값을 무시함.
+  joint는 0, 1, 2 값을 가지며 각각 elbow, hip, knee를 나타냄
+  """
+  if len(angle_list) < 5:
+    return False
+  
+  th = [35, 25]
+
+  # 가장 최근 값 3개와 새로운 값이 차이가 너무 크면 outlier
+  if abs((angle_list[-2]+angle_list[-3]+angle_list[-4])/3 - angle_list[-1]) > th[joint]:
+      return True
+  return False
 
 
 @torch.no_grad()
@@ -19,7 +34,7 @@ def run(
         yolo_weights = 'yolov7-w6-pose.pt',
         source = 'test.mp4',
         device = 'cpu',
-        curl_traker = True,
+        tracker = True,
         drawskeleton = False
 ):      
     
@@ -40,7 +55,7 @@ def run(
         fw, fh = int(cap.get(3)), int(cap.get(4))
         if player.isnumeric():
             webcam = True
-            fw, fh = 1280, 640
+            fw, fh = 640, 320
         
         vid_write_image = letterbox(
             cap.read()[1], (fw), stride=64,auto=True)[0]
@@ -52,17 +67,16 @@ def run(
         
         elbow_state = 'up'
         Is_Start = False
+        temp = {'elbow_right':[],'knee_right':[]}
+        pushups = []
         
         frame_count, total_fps = 0, 0
         
         bcount = 0
         direction = 0
         
-        
         fontpath = "BMJUA_otf.otf"
         font = ImageFont.truetype(fontpath, 32)
-        font1 = ImageFont.truetype(fontpath, 32)
-        
         
         while True:
             ## yolov7 keypoint baseline start
@@ -98,83 +112,83 @@ def run(
             ## yolov7 keypoint baseline end
                 
                 ## push up estimation
-                temp = {'elbow_right':[],'knee_right':[]}
-                pushups = []
                 
-                if curl_traker :
+                im = Image.fromarray(img)
+                draw = ImageDraw.Draw(im)
+                draw.text((30,10), f"{len(pushups)}", font=font, fill=(0,0,0))
+                img = np.array(im)
+                
+                if tracker :
                     for idx in range(output.shape[0]):
                         
-                        
                         ## estimate elbow up and down
-                        elbow_right_angle = findAngle(img, output[idx, 7:].T,5,7,9, draw=True)
+                        elbow_right_angle = Angle(img, output[idx, 7:].T,5,7,9, draw=True)
                         Is_elbow_up = elbow_right_angle > 130
                         Is_elbow_down = elbow_right_angle < 110
                         temp['elbow_right'].append(elbow_right_angle)
                         print(f'elbow angle {elbow_right_angle}')
                         
-                        ## estimate hip up and down
-                        # hip_right_angle = findAngle(img, output[idx, 7:].T,11,12,11, draw=True)
-                        # hip_condition = (hip_right_angle >140) and (hip_right_angle < 220)
-                        # print(f'hip angle {hip_right_angle} ')
-                        
                         ## estimate knee up and down
-                        knee_right_angle = findAngle(img, output[idx, 7:].T,11,13,15, draw=True)
-                        knee_condition = (knee_right_angle > 130) and (knee_right_angle < 205)
+                        knee_right_angle = Angle(img, output[idx, 7:].T,11,13,15, draw=True)
+                        knee_condition = (knee_right_angle > 130) and (knee_right_angle < 220)
                         temp['knee_right'].append(knee_right_angle)
                         print(f'knee angle : {knee_right_angle}')
                         
                         
                         lower_body_condition =  knee_condition
                         
-                        
-                        Is_pushup_angle = (elbow_right_angle > 140)  and  (knee_right_angle > 125)   
+                        Is_pushup_angle = (elbow_right_angle > 140)  and  (knee_right_angle > 125)  
                         if Is_pushup_angle :
                             Is_Start = True
                             print("success")
+                            
                         if not Is_Start :
                             temp['elbow_right'].pop()
                             temp['knee_right'].pop()
                         else :
-                            print('temp elbow :',temp['elbow_right'])
-                            print('knee right :',temp['knee_right'])
-                            if Is_elbow_up and elbow_state == "down" and lower_body_condition :
-                                
-                                pushup_endtime = time.time()
-                                elbow_state = "up"
-                                element = []
-                                
-                                if max(temp['elbow_right']) > 160:
-                                    element.append(1)
-                                else :
-                                    element.append(0) 
+                            if IsOutlier_pushups(temp['elbow_right'], 0) or IsOutlier_pushups(temp['knee_right'], 1) :
+                                temp['elbow_right'].pop()
+                                temp['knee_right'].pop()
+                            else :  
+                                if Is_elbow_up and  lower_body_condition :
+                                    print("lower_body_condition")
+                                    pushup_endtime = time.time()
+                                    elbow_state = "up"
+                                    element = []
                                     
-                                if min(temp['elbow_right']) < 70:
-                                    element.append(1)
-                                else:
-                                    element.append(0)
+                                    if max(temp['elbow_right']) > 160:
+                                        element.append(1)
+                                    else :
+                                        element.append(0) 
+                                        
+                                    if min(temp['elbow_right']) < 70:
+                                        element.append(1)
+                                    else:
+                                        element.append(0)
+                                    
+                                    temp['elbow_right'] = []
+                                    
+                                    if min(temp['knee_right']) < 130:
+                                        element.append(0)
+                                    else:
+                                        element.append(1)
+                                    
+                                    temp['knee_right'] = []
+                                    
+                                    pushups.append(element)
                                 
-                                temp['elbow_right'] = []
-                                
-                                if min(temp['knee_right']) < 130:
-                                    element.append(0)
-                                else:
-                                    element.append(1)
-                                
-                                temp['knee_right'] = []
-                                
-                                pushups.append(element)
                                 
                             
-                            elif Is_elbow_down and elbow_state == 'up' and lower_body_condition:
-                                pushup_starttime = time.time()
-                                elbow_state = 'down'
+                                elif Is_elbow_down and elbow_state =='up' and lower_body_condition:
+                                    pushup_starttime = time.time()
+                                    elbow_state = 'down'
+                            
+                            
+                            print("pushups",len(pushups))
+                            
                                 
-                            im = Image.fromarray(img)
-                            draw = ImageDraw.Draw(im)
-                            draw.text((145,100), f"{int(len(pushups))}%", font=font, fill=(255,255,255))
+                            
                     
-                            
-                        
                         percentage = np.interp(elbow_right_angle, (210,290), (0,100))
                         bar = np.interp(elbow_right_angle, (220,290), (int(fh)-100,100))
                         
@@ -193,27 +207,6 @@ def run(
                                 bcount += 0.5
                                 direction = 0
                                 
-                        # cv2.line(img,(100,100),(100, int(fh)-100),(255,255,255), 30)
-                        # cv2.line(img,(100,int(bar)),(100, int(fh)-100), color, 30)
-                        
-                        # if (int(percentage) < 10):
-                        #     cv2.line(img, (155, int(bar)), (190,int(bar)),color,40)
-                        # elif((int(percentage) >= 10) and (int(percentage)>100)):
-                        #     cv2.line(img, (155, int(bar)), (200,int(bar)),color,40)
-                        # else:
-                        #     cv2.line(img, (155, int(bar)), (210,int(bar)),color,40)
-                            
-                        # im = Image.fromarray(img)
-                        # draw = ImageDraw.Draw(im)
-                        # draw.rounded_rectangle((fw-300, (fh//2)- 100, fw-50, (fh//2) * 100),fill = color, radius= 40)
-
-                        # draw.text(
-                        #     (145,100), f"{int(percentage)}%", font=font, fill=(255,255,255))
-                    
-                        # draw.text(
-                        #     (fw-230,(fh//2)-100), f"{int(bcount)}%", font=font1, fill=(255,255,255))
-
-                        # img = np.array(im)
                     
                 if drawskeleton : 
                     for idx in range(output.shape[0]):
@@ -236,7 +229,6 @@ def run(
                 fps = 1 / (end_time - start_time)
                 total_fps += fps
                 frame_count += 1
-                # out.write(img)
                 print(f'estimation:({end_time-start_time:.3f}s)')
                 
             else:
@@ -245,6 +237,8 @@ def run(
         cap.release()
         avg_fps = total_fps / frame_count
         print(f"Average FPS : {avg_fps}")
+
+
                     
         
 def parse_opt():
